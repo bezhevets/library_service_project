a93e365c7864a59ca5b7b7e6c950efc4c033dc36
Maksym Bezhevets <begik4@gmail.com>
2024-01-30 22:24:35 +0200

    Optimized queries to Borrowing.

--- borrowing/views.py
+++ borrowing/views.py
@@ -5,7 +5,7 @@ from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.all()
+    queryset = Borrowing.objects.all().select_related("user", "book")
     serializer_class = BorrowingSerializer
 
     def get_serializer_class(self):
