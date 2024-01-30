fe768d98e2e3328a3e073aa02face82227ab31c5
Maksym Bezhevets <begik4@gmail.com>
2024-01-30 17:23:43 +0200

    Created Borrowing model, and registered in admin panel.

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from book_service.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_data = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.user.full_name} borrowing {self.book.title}"
