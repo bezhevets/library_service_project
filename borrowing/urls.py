97b04f05e574fbdbbf054cdd0fb255b7495ffccb
Maksym Bezhevets <begik4@gmail.com>
2024-01-31 00:44:37 +0200

    Updated urls in borrowing.

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from borrowing.views import BorrowingViewSet
 
 router = routers.DefaultRouter()
-router.register("borrowings", BorrowingViewSet)
+router.register("", BorrowingViewSet)
 
 
 urlpatterns = router.urls
