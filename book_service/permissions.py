3b69f90fc87d07dbcaf5890296053f6dcddc2575
Maksym Bezhevets <begik4@gmail.com>
2024-01-30 15:17:57 +0200

    Created permissions, added to book viewset.

---
+++ book_service/permissions.py
@@ -0,0 +1,21 @@
+from rest_framework.permissions import (
+    BasePermission,
+    SAFE_METHODS,
+    DjangoObjectPermissions,
+)
+
+
+class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        )
+
+
+class AnonReadOnly(DjangoObjectPermissions):
+    authenticated_users_only = False
--- book_service/views.py
+++ book_service/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 
 from book_service.models import Book
+from book_service.permissions import AnonReadOnly
 from book_service.serializers import BookSerializer
 
 
@@ -8,3 +9,4 @@ from book_service.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (AnonReadOnly,)
