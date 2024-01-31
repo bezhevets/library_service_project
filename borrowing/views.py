84da2337b69e69c64c7f8dcb5011ab8d230c27cc
Maksym Bezhevets <begik4@gmail.com>
2024-01-31 12:28:40 +0200

    Added filtering for admin by user_ia and is_active. Filtering for the active user can see only their borrowings.

--- borrowing/views.py
+++ borrowing/views.py
@@ -9,6 +9,18 @@ class BorrowingViewSet(viewsets.ModelViewSet):
     serializer_class = BorrowingSerializer
     permission_classes = [IsAdminOrIfAuthenticatedBorrowingPermission]
 
+    def get_queryset(self):
+        queryset = self.queryset
+        if self.request.user.is_staff:
+            is_active = self.request.query_params.get("is_active")
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user_id=int(user_id))
+            if is_active == "true":
+                queryset = queryset.filter(actual_return_data__isnull=True)
+            return queryset
+        return queryset.filter(user=self.request.user)
+
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
             return BorrowingListSerializer
