89e775fe34b10d3d15f7af0754115276f8f12fcb
Maksym Bezhevets <begik4@gmail.com>
2024-01-31 15:27:21 +0200

    Added action return, and functionality for returning books.

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,4 +1,10 @@
-from rest_framework import viewsets
+from datetime import date
+
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from borrowing.models import Borrowing
 from borrowing.permissions import IsAdminOrIfAuthenticatedBorrowingPermission
 from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingCreateSerializer
@@ -30,3 +36,26 @@ class BorrowingViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(
+        methods=["GET"],
+        detail=True,
+        url_path="return",
+    )
+    def return_book(self, request, pk=None):
+        borrowing = self.get_object()
+
+        if borrowing.actual_return_data:
+            return Response(
+                {"detail": "This borrowing has already been returned."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        borrowing.actual_return_data = date.today()
+        borrowing.book.inventory += 1
+        borrowing.book.save()
+        borrowing.save()
+        return Response(
+            {"detail": "This book was successfully returned."},
+            status=status.HTTP_200_OK
+        )
