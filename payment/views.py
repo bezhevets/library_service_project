a4d91ba03211a0347008f7d42fea41adc5c373cf
Maksym Bezhevets <begik4@gmail.com>
2024-02-01 17:10:50 +0200

    Created ViewSet, filtering admin and non admin. Written doc string.

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import PaymentSerializer, PaymentListSerializer, PaymentDetailSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.select_related("borrowing")
+    serializer_class = PaymentSerializer
+
+    def get_queryset(self):
+        """
+        Get the queryset of payments based on user role.
+
+        For admins, all payments are retrieved.
+        For regular users, only their payments are retrieved.
+
+        Returns:
+        - queryset: Filtered queryset based on user's role.
+        """
+        queryset = Payment.objects.select_related("borrowing")
+
+        if self.action == "list":
+            if not self.request.user.is_staff:
+                queryset = (
+                    queryset.filter(borrowing_id__user_id=self.request.user)
+                )
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+        return PaymentSerializer
