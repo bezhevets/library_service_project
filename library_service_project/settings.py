656ae35d1bd7d10b5e5b85bcd82ce83b7d128f7a
Maksym Bezhevets <begik4@gmail.com>
2024-02-03 22:00:56 +0200

    Added default pagination.

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -150,6 +150,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "book_service.permissions.IsAdminOrIfAuthenticatedReadOnly",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
 }
 
 SIMPLE_JWT = {
