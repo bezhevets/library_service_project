6bb51c54c1af97d8c431074aa93a5e5a95a5bcea
Maksym Bezhevets <begik4@gmail.com>
2024-02-03 17:16:25 +0200

    Rewritten the serializers and added payments info

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -6,44 +6,13 @@ from borrowing.models import Borrowing
 from payment.models import Payment
 
 from borrowing.tasks import notification_new_borrowing
+from payment.serializers import PaymentDetailSerializer
 from payment.stripe_helper import create_checkout_session
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Borrowing
-        fields = (
-            "id",
-            "book",
-            "user",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_data",
-        )
-        read_only_fields = ("id", "borrow_date", "actual_return_data")
-
-
-class BorrowingListSerializer(BorrowingSerializer):
-    book = BookSerializer()
-    user = serializers.SlugRelatedField(
-        many=False, read_only=True, slug_field="full_name"
-    )
-
-    class Meta:
-        model = Borrowing
-        fields = (
-            "id",
-            "book",
-            "user",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_data",
-        )
-
-
-class BorrowingCreateSerializer(BorrowingSerializer):
     def validate(self, attrs):
-        data = super(BorrowingCreateSerializer, self).validate(attrs)
+        data = super(BorrowingSerializer, self).validate(attrs)
         Borrowing.valid_inventory_book(
             attrs["book"].inventory,
             serializers.ValidationError,
@@ -60,8 +29,6 @@ class BorrowingCreateSerializer(BorrowingSerializer):
             borrowing.book.inventory -= 1
             borrowing.book.save()
 
-            notification_new_borrowing.delay(borrowing.id)
-
             session = create_checkout_session(
                 borrowing, self.context["request"]
             )
@@ -75,4 +42,42 @@ class BorrowingCreateSerializer(BorrowingSerializer):
                 money_to_pay=session.amount_total / 100,
             )
 
+            notification_new_borrowing.delay(borrowing.id)
         return borrowing
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    book = serializers.SlugRelatedField(many=False, read_only=True, slug_field="title")
+    user = serializers.SlugRelatedField(
+        many=False, read_only=True, slug_field="full_name"
+    )
+    payments = serializers.SlugRelatedField(many=True, read_only=True, slug_field="status")
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "user",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_data",
+            "payments"
+        )
+
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = BookSerializer()
+    payments = PaymentDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "user",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_data",
+            "payments"
+        )
